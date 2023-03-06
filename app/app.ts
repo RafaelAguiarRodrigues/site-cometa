93fe6205a1db9f82eea192777b6a453d3d7561eb
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2023-03-06 20:17:42 -0300

    terminando o bruto da pagina

--- app/app.ts
+++ app/app.ts
@@ -1,28 +1,34 @@
 const slides = document.querySelectorAll('[data-js="carousel__item"]');
 const proxBotao = document.querySelector('[data-js="carousel__button--next"]');
 const voltaBotao = document.querySelector('[data-js="carousel__button--prev"]');
+
 let currentSlideIndex = 0;
+
 proxBotao.addEventListener('click', () => {
-    if (currentSlideIndex === slides.length - 1) {
+    if(currentSlideIndex === slides.length - 1) {
         currentSlideIndex = 0;
-    }
-    else {
+    } else {
         currentSlideIndex++;
     }
+
     slides.forEach(slide => {
         slide.classList.remove('carousel__item--visible');
-    });
+    })
+
     slides[currentSlideIndex].classList.add('carousel__item--visible');
-});
+})
+
 voltaBotao.addEventListener('click', () => {
-    if (currentSlideIndex === 0) {
+    if(currentSlideIndex === 0){
         currentSlideIndex = slides.length - 1;
-    }
-    else {
+    } else {
         currentSlideIndex--;
     }
+    
+
     slides.forEach(slide => {
-        slide.classList.remove('carousel__item--visible');
-    });
-    slides[currentSlideIndex].classList.add('carousel__item--visible');
-});
+        slide.classList.remove('carousel__item--visible')
+    })
+
+    slides[currentSlideIndex].classList.add('carousel__item--visible')
+})
